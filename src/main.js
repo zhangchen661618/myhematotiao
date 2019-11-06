ff60f644d12763095ea4c222ffbb214f7baac189
张晨 <502030647@example.com>
2019-11-06 22:35:03 +0800

    整理目录-准备资源-引入重置的样式

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import Element from 'element-ui' // 引入elementui组件
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
-
+import './style/index.less' // 引入重置的基础样式
 Vue.use(Element) // 注册 element 组件
 Vue.config.productionTip = false
 
