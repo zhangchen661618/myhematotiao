b4d56d9679884f6b13bff1e95995d4c8e1bb54b7
张晨 <502030647@example.com>
2019-11-06 22:21:01 +0800

    安装elementUI并注册使用

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Element from 'element-ui' // 引入elementui组件
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 
+Vue.use(Element) // 注册 element 组件
 Vue.config.productionTip = false
 
 new Vue({
