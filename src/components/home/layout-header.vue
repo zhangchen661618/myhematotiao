528e98b14e7d14509af6631bc4a12a2a513f1db6
张晨 <502030647@example.com>
2019-11-09 18:02:08 +0800

    home主页-头部信息查询及图片base64

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -6,11 +6,11 @@
             <span>江苏传智播客教育科技股份有限公司</span>
         </el-col>
         <el-col :span="3" class="box-row-col2" >
-            <img  src="../../assets/img/avatar.jpg" alt="">
+            <img  :src="user.photo ? user.photo : defaultImg" alt="">
             <el-dropdown trigger="click">
                 <!-- 匿名插槽 -->
                 <span class="el-dropdown-link">
-                    我是用户名
+                    {{user.name}}
                     <i class="el-icon-arrow-down el-icon--right"></i>
                 </span>
                 <!-- 具名插槽 -->
@@ -26,7 +26,30 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      user: {},
+      defaultImg: require('../../assets/img/avatar.jpg') // require的图片就变成了 base64
+    }
+  },
+  methods: {
+    // 获取用户信息
+    getUserInfo () {
+      let userInfo = window.localStorage.getItem('user-info')
+      let token = userInfo ? JSON.parse(userInfo).token : null // 获取token
+      token && this.$axios({
+        method: 'get',
+        url: '/user/profile',
+        headers: { 'Authorization': `Bearer ${token}` }// 将headers中赋值 后端需要的token身份信息 携带令牌
+      }).then((result) => {
+        // console.log(result.data.data)
+        this.user = result.data.data // 获取到用户的最新资料，赋值给data中的数据
+      })
+    }
+  },
+  created () {
+    this.getUserInfo() // 获取用户信息
+  }
 }
 </script>
 
