325551e38d8ac898817a030d8f2da8247614db7a
张晨 <502030647@example.com>
2019-11-27 16:56:00 +0800

    主页模块-默认导航

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home/index.vue'
 import Login from '../views/login/index.vue'
-
+import Main from '../views/home/main.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -18,7 +18,13 @@ const routes = [
   {
     path: '/home',
     name: 'home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: '',
+        component: Main // 默认的二级路由
+      }
+    ]
   }
   // {
   //   path: '/about',
