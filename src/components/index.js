429c45ffb97601e767b13838bfd9f527fee3766b
张晨 <502030647@example.com>
2019-12-01 15:56:45 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
+import breadCrumb from './common/bread-crumb'
 import Vue from 'vue'
 Vue.component('layout-aside', layoutAside)
 Vue.component('layout-header', layoutHeader)
@@ -8,7 +9,8 @@ Vue.component('layout-header', layoutHeader)
 // vue.use(elementUI) 时会调用提供的install方法，并且在方法中传入vue对象
 export default {
   insatll (Vue) {
-    Vue.component('layout-header', layoutHeader)
-    Vue.component('layout-aside', layoutAside)
+    Vue.component('layout-header', layoutHeader) // 注册 layout-header组件
+    Vue.component('layout-aside', layoutAside) // 注册左侧菜单组件
+    Vue.component('bread-crumb', breadCrumb) // 注册面包屑组件
   }
 }
