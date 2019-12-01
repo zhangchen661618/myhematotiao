6af16760d2c13dc82db03cc92e30d9af16a9d65a
张晨 <502030647@example.com>
2019-12-01 22:37:03 +0800

    统一处理大数字类型

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -45,7 +45,7 @@ export default {
         this.$axios({
           method: 'put',
           url: '/comments/status',
-          params: { article_id: row.id },
+          params: { article_id: row.id.toString() },
           data: { allow_comment: !row.comment_status }// 状态是反着的
         }).then(() => {
           // 如果进去到then函数中一定是成功
