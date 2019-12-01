d686a680544edfdbedbfe0b80b43e96f3d433cc6
张晨 <502030647@example.com>
2019-12-02 02:15:00 +0800

    素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -16,13 +16,34 @@
                         </el-row>
                     </el-card>
                 </div>
+                 <el-row type="flex" justify="center">
+                        <el-pagination
+                        :current-page="page.currentPage"
+                        :page-size="page.pageSize"
+                        :total="page.total"
+                        @current-change="changePage"
+                        background
+                        layout="prev, pager, next">
+                        </el-pagination>
+                    </el-row>
             </el-tab-pane>
+
             <el-tab-pane label="收藏图片" name="collect">
                 <div class="img-list">
                     <el-card class="img-card" v-for="item in list" :key="item.id">
                         <img :src="item.url" alt=""/>
                     </el-card>
                 </div>
+                <el-row type="flex" justify="center">
+                        <el-pagination
+                        :current-page="page.currentPage"
+                        :page-size="page.pageSize"
+                        :total="page.total"
+                        @current-change="changePage"
+                        background
+                        layout="prev, pager, next">
+                        </el-pagination>
+                    </el-row>
             </el-tab-pane>
         </el-tabs>
     </el-card>
@@ -33,25 +54,42 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        currentPage: 1,
+        pageSize: 15,
+        total: 0
+      }
     }
   },
   methods: {
+    // 切换页码
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getMaterial()
+    },
+    // 切换页签
     changeTab () {
+      this.page.currentPage = 1
       this.getMaterial()
     },
     getMaterial () {
+      let pageParams = {
+        page: this.page.currentPage,
+        per_page: this.page.pageSize
+      }
       this.$axios({
         method: 'get',
         url: '/user/images',
-        params: { collect: this.activeName === 'collect' }
+        params: { collect: this.activeName === 'collect', ...pageParams }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
   },
   created () {
-    this.getMaterial()// 第一次进去 getMaterial===all
+    this.getMaterial()// 第一次进去 getMaterial===all 加载第一页的全部数据
   }
 }
 </script>
