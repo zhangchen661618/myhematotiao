0918a5219232d3dcf677b05f4cb0ceee49ce84c1
张晨 <502030647@qq.com>
2019-12-05 22:15:46 +0800

    发表文章-页面结构-简单实现/频道数据加载

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,15 +3,69 @@
       <bread-crumb slot="header">
           <template slot="title">发布文章</template>
       </bread-crumb>
+      <el-form class="publish-form" label-width="80px">
+          <el-form-item label="标题">
+              <el-input style="width:400px;"></el-input>
+          </el-form-item>
+          <el-form-item label="内容">
+              <el-input
+                type="textarea"
+                :rows="4"
+                placeholder="请输入内容"
+                v-model="textarea">
+              </el-input>
+          </el-form-item>
+          <el-form-item label="封面">
+              <el-radio-group>
+                  <el-radio>单图</el-radio>
+                  <el-radio>三图</el-radio>
+                  <el-radio>无图</el-radio>
+                  <el-radio>自动</el-radio>
+              </el-radio-group>
+          </el-form-item>
+          <el-form-item label="频道">
+            <el-select>
+                <el-option
+                :label="item.name"
+                :value="item.id"
+                v-for="item in channels"
+                :key="item.id">
+                </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+              <el-button type="primary">发布</el-button>
+              <el-button>存入草稿</el-button>
+          </el-form-item>
+      </el-form>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      channels: []
+    }
+  },
+  methods: {
+    getChannels () {
+      this.$axios({
+        method: 'get',
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels() // 获取频道列表
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.publish-form{
+    margin-left: 50px;
+}
 </style>
