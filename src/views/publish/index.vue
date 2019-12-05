3700a64307c566fa6b37deb1f1f973965a7d29b0
张晨 <502030647@qq.com>
2019-12-05 22:49:03 +0800

    发布文章-发布文章检验逻辑实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,28 +3,28 @@
       <bread-crumb slot="header">
           <template slot="title">发布文章</template>
       </bread-crumb>
-      <el-form class="publish-form" label-width="80px">
-          <el-form-item label="标题">
-              <el-input style="width:400px;"></el-input>
+      <el-form ref="myForm" :rules="rules" :model="formData" class="publish-form" label-width="80px">
+          <el-form-item prop="title" label="标题">
+              <el-input v-model="formData.title" style="width:400px;"></el-input>
           </el-form-item>
-          <el-form-item label="内容">
+          <el-form-item prop="content" label="内容">
               <el-input
+                v-model="formData.content"
                 type="textarea"
                 :rows="4"
-                placeholder="请输入内容"
-                v-model="textarea">
+                placeholder="请输入内容">
               </el-input>
           </el-form-item>
           <el-form-item label="封面">
-              <el-radio-group>
-                  <el-radio>单图</el-radio>
-                  <el-radio>三图</el-radio>
-                  <el-radio>无图</el-radio>
-                  <el-radio>自动</el-radio>
+              <el-radio-group v-model="formData.cover">
+                  <el-radio :label="1">单图</el-radio>
+                  <el-radio :label="3">三图</el-radio>
+                  <el-radio :label="0">无图</el-radio>
+                  <el-radio :label="-1">自动</el-radio>
               </el-radio-group>
           </el-form-item>
-          <el-form-item label="频道">
-            <el-select>
+          <el-form-item label="频道" prop="channel_id">
+            <el-select v-model="formData.channel_id">
                 <el-option
                 :label="item.name"
                 :value="item.id"
@@ -34,7 +34,7 @@
             </el-select>
           </el-form-item>
           <el-form-item>
-              <el-button type="primary">发布</el-button>
+              <el-button @click="publish" type="primary">发布</el-button>
               <el-button>存入草稿</el-button>
           </el-form-item>
       </el-form>
@@ -45,10 +45,30 @@
 export default {
   data () {
     return {
-      channels: []
+      channels: [],
+      formData: {
+        title: '',
+        content: '',
+        channel_id: null,
+        cover: 0 // 默认给个无图
+      },
+      rules: {
+        title: [{ required: true, message: '标题内容不得为空' }],
+        content: [{ required: true, message: '文章内容不得为空' }],
+        channel_id: [{ required: true, message: '频道不得为空' }]
+      }
     }
   },
   methods: {
+    // 发布内容
+    publish () {
+      this.$refs.myForm.validate((isOk) => {
+        if (isOk) {
+          // 发布文章
+        }
+      })
+    },
+    // 获取频道列表
     getChannels () {
       this.$axios({
         method: 'get',
